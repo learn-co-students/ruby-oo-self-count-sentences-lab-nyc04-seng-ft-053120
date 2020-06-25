9ef2a91df47e10c53f10be3e484471893df24f73
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-06-25 15:21:11 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") #to determine what the final character in a string is
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #string to array with each word as an element
+    #remove special characters any and all periods, question marks and exclamation marks.
+    #count each word
+    #return an integer
+    new_array = self.split(/[.?!]/).reject{|string| string.empty?}
+    return new_array.length
   end
 end
